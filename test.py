74acb125a80adacb47b06d1d935ea3604ff2b519
xballes <70892571+xballes@users.noreply.github.com>
2023-11-14 18:11:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from wallet import Wallet
 def test_getbalance():
     obj = Wallet(0)
     obj.set_balance(20)
-    assert obj.get_balance() == 30
+    assert obj.get_balance() == 20
 
 def test_removebalance():
     obj = Wallet(50)
